caa6863086034fde0c044f6935a81decb0a5ddab
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-26 16:41:33 -0600

    Routing con React Router DOM 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+// Layouts
+import AuthLayout from './Layouts/AuthLayout.jsx'
+
+// Paginas
+import Login from './Pages/Login.jsx'
+import Registrar from './Pages/Registrar.jsx'
+import OlvidePassword from './Pages/OlvidePassword.jsx'
+import NuevoPassword from './Pages/NuevoPassword.jsx'
+import ConfirmarCuenta from './Pages/ConfirmarCuenta.jsx'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Uptask</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout/>}>
+          <Route index element={<Login/>}/>
+          <Route path='registrar' element={<Registrar/>}/>
+          <Route path='olvide-password' element={<OlvidePassword/>}/>
+          <Route path='olvide-password/:token' element={<NuevoPassword/>}/>
+          <Route path='confirmar-cuenta/:token' element={<ConfirmarCuenta/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
