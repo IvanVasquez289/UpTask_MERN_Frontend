760d209e559e5853e5239ed613e4ebf70227bc0a
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-26 19:31:46 -0600

    Crear pagina de olvide password

--- src/Pages/OlvidePassword.jsx
+++ src/Pages/OlvidePassword.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
 const OlvidePassword = () => {
   return (
-    <div>OlvidePassword</div>
-  )
-}
+    <>
+      <h1 className=" text-sky-600 font-black text-6xl capitalize mt-10">
+        Inicia sesión y administra tus {""}
+        <span className=" text-gray-700">proyectos</span>
+      </h1>
+      <form className=" bg-white p-10 shadow rounded-lg my-10">
+        <div className="mb-7">
+          <label
+            htmlFor="email"
+            className=" text-gray-700 text-xl font-bold uppercase"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email de registro"
+            className="w-full mt-3 bg-gray-100 p-4 rounded-xl border"
+          />
+        </div>
+       
+        <input
+          type="submit"
+          value="Enviar Instrucciones"
+          className="w-full bg-sky-700 py-3 rounded-xl text-white font-bold uppercase cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
 
-export default OlvidePassword
+      <nav className=" xl:flex xl:justify-between">
+        <Link
+          to={"/registrar"}
+          className="text-slate-500 uppercase block text-center text-sm my-3"
+        >
+          No tienes cuenta? Registrate
+        </Link>
+        <Link
+          to={"/"}
+          className="text-slate-500 uppercase block text-center text-sm my-3"
+        >
+          Ya tienes una cuenta? Inicia Sesión
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default OlvidePassword;
