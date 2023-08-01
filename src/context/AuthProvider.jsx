10539fd53c8b9241f6126bdfe65cf3d96be25507
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-08-01 00:10:25 -0600

    Revisando token para acceder al perfil del usuario con JWT

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,39 @@
 import { useState, useEffect, createContext } from "react";
+import clienteAxios from "../config/clienteAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
 
     const [auth,setAuth] = useState({})
+
+    useEffect(() => {
+        const comprobarToken = async () => {
+            const token = localStorage.getItem('token')
+            
+            if(!token){
+                return
+            }
+
+            const config = {
+                headers: {
+                    "Content-Type": "Application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            try {
+                const {data} = await clienteAxios('/usuarios/perfil',config)
+               setAuth(data)
+            } catch (error) {
+                console.log(error.response.data.msj)
+            }
+
+        }
+
+        comprobarToken()
+    }, [])
+    
 
     return (
         <AuthContext.Provider
