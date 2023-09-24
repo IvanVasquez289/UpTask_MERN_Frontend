d93d6f895dce7a37825b1a45cac92da5a5863710
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-09-24 01:28:32 -0600

    Mostrar leyenda colaborador

--- src/Components/ProyectoPreview.jsx
+++ src/Components/ProyectoPreview.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 const ProyectoPreview = ({ proyecto }) => {
-  const { nombre, _id, cliente } = proyecto;
+  const {auth} = useAuth()
+  const { nombre, _id, cliente ,creador} = proyecto;
+
   return (
-    <div className=" p-5 border-b flex items-center">
-      <p className="flex-1">
-        {nombre}{" "}
-        <span className="text-gray-500 uppercase text-sm">{cliente}</span>{" "}
-      </p>
+    <div className=" p-5 border-b flex items-center justify-between">
+      <div className="flex-col md:flex md:flex-row items-center gap-2">
+        <p>
+          {nombre}
+        </p>
+
+        <p className="text-gray-500 uppercase text-sm italic">{cliente}</p>{" "}
+
+        {auth._id !== creador && (
+          <p className="bg-green-400 p-1 rounded text-white text-sm font-bold">
+            Colaborador
+          </p>
+        )}
+      </div>
 
       <Link
         to={`${_id}`}
